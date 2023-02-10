d882108baee698c5a6312acfcfbc8da59a88627b
Aymen Diabi <aymendiabi@gmail.com>
2023-02-10 23:59:40 +0100

    00

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {
-      colors: {
-        darkBlue: '#0B0D17',
-        lightBlue: '#D0D6F9',
-      },
-      fontFamily: {
-        serif: ['Bellefair', 'serif'],
-        sans: ['Barlow Condensed', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
