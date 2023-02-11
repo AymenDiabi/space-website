36ffcfe299abb5c892d3346aa9f75efae23d72fa
Aymen Diabi <aymendiabi@gmail.com>
2023-02-11 02:28:22 +0100

    There is a bug in the navigation divider

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: '#0B0D17',
+        lightkBlue: '#D0D6F9'
+      },
+      fontFamily: {
+        sans: ['Barlow Condensed', 'sans-serif'],
+        serif: ['Bellefair', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
